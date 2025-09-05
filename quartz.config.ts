83612b949b43df224f62437a27ef0c7a9eabbf31
Devansh Mahajan <mahajandevansh@icloud.com>
2025-09-06 00:32:56 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "999", "900", "600", "100", "000"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
