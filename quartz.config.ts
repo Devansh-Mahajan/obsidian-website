5eadfb43da50b740789df2fd794bcdb13c5955db
Devansh Mahajan <mahajandevansh@icloud.com>
2025-09-07 23:04:49 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Devansh Mahajan's Study Notes",
+    pageTitle: "Dash's Study Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
